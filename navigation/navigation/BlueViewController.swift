2e4c0d4e255a9dc89264605ea55db6cfdd076083
Jayeshrajole <110555604+Jayeshrajole@users.noreply.github.com>
2022-09-12 09:55:40 +0530

    Add files via upload

---
+++ navigation/navigation/BlueViewController.swift
@@ -0,0 +1,21 @@
+//
+//  BlueViewController.swift
+//  navigation
+//
+//  Created by Jayesh Rajole on 05/09/22.
+//
+
+import UIKit
+
+class BlueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Blue screen"
+
+    }
+    
+
+  
+
+}
---
+++ navigation/navigation/YellowViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TealViewController.swift
+//  navigation
+//
+//  Created by Jayesh Rajole on 05/09/22.
+//
+
+import UIKit
+
+class TealViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Yellow screen"
+
+    }
+    
+
+    
+}
